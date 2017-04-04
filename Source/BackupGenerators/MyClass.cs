77a29ef5d3027c7adc19cfad751be06c4e1f1d07
Jake Vose <jake.vose@gmail.com>
2017-04-04 10:21:38 -0400

    initial commit

---
+++ Source/BackupGenerators/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mephistopheles
+ * Date: 4/4/2017
+ * Time: 10:10 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace BackupGenerators
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
