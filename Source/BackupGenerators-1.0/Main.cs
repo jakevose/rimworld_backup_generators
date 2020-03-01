732f342c89734a43054b0bedeb1e098c1d9208c4
Jake Vose <jake.vose@gmail.com>
2020-03-01 09:21:04 -0500

    phew, try to make building for 1.0 easier next time

---
+++ Source/BackupGenerators-1.0/Main.cs
@@ -0,0 +1,54 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace BackupGenerators
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = HarmonyInstance.Create("com.backupgenerators.rimworld.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Message("BackupGenerators: Adding Harmony Postfix to GameComponentUtility.StartedNewGame");
+            Log.Message("BackupGenerators: Adding Harmony Postfix to GameComponentUtility.LoadedGame");
+        }
+    }
+
+    [HarmonyPatch(typeof(GameComponentUtility), "StartedNewGame")]
+    static class Patch_GameComponentUtility_StartedNewGame
+    {
+        static void Postfix()
+        {
+			if (Settings.LowPowerThresh.AsFloat == 1 && Settings.HighPowerThresh.AsFloat == 1)
+            {
+                BackupGeneratorsSettingsUtil.ApplyFactor(200, 1000);
+            }
+            else
+            {
+                BackupGeneratorsSettingsUtil.ApplyFactor(Settings.LowPowerThresh.AsFloat, Settings.HighPowerThresh.AsFloat);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameComponentUtility), "LoadedGame")]
+    static class Patch_GameComponentUtility_LoadedGame
+    {
+        static void Postfix()
+        {
+			if (Settings.LowPowerThresh.AsFloat == 1 && Settings.HighPowerThresh.AsFloat == 1)
+            {
+                BackupGeneratorsSettingsUtil.ApplyFactor(200, 1000);
+            }
+            else
+            {
+                BackupGeneratorsSettingsUtil.ApplyFactor(Settings.LowPowerThresh.AsFloat, Settings.HighPowerThresh.AsFloat);
+            }
+        }
+    }
+}
